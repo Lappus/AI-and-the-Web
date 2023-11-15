61d34bd50081c7832907f6daf0ee1b63a00e6332
Lappus <juli.cornelsen@googlemail.com>
2023-11-15 23:23:49 +0100

    HTML

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from crawler_mit_whoosh import request, search
+
+app = Flask(__name__)
+@app.route('/')
+
+def home():
+    return render_template('home.html')
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    if request.method =='GET':
+        query = request.args.get('q')
+        if query:
+            matches = search(query.split())
+            return render_template("search.html", matches=matches, query=query)
+        else:
+            return "Please enter a query"
+    
+    return render_template('search.html')
+    
+if __name__ == "__main__":
+    app.run(debug=True)
